90a4545587e4f5dd1d2cb7882a66670389397d83
DeepakJayakumarSnowflake <103590662+DeepakJayakumarSnowflake@users.noreply.github.com>
2022-04-13 22:51:16 +0530

    Update streamlit_app.py
    correcting typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ streamlit.header('Create your own smooothie')
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list =  my_fruit_list.set_index('Fruits')
+my_fruit_list =  my_fruit_list.set_index('Fruit')
 
 
 # creating a multiselect to allow user to select their fruit as they wish
