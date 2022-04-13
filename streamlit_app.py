077fc2456143a3faddfe059da98abe86cd809095
DeepakJayakumarSnowflake <103590662+DeepakJayakumarSnowflake@users.noreply.github.com>
2022-04-13 22:48:18 +0530

    Update streamlit_app.py
    adding mutiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,9 +9,15 @@ streamlit.header('BreakFast')
 streamlit.text('Idly')
 streamlit.text('Dosa')
 streamlit.text('Pongal')
-streamlit.header('Special mini Tiffin as per your request')
+streamlit.header('Create your own smooothie')
 
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+
+
+# creating a multiselect to allow user to select their fruit as they wish
+streamlit.multiselect("Pick Some Fruits",list(my_fruit_list.index))
+
 streamlit.dataframe(my_fruit_list)
